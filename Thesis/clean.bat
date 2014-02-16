95a20c65a92490b8ca6ef06303a349f42cc1656e
sky chan <diufanshu@gmail.com>
2014-02-16 11:11:04 +0800

    beta v 2.7
    测试可用 继续维护

---
+++ Thesis/clean.bat
@@ -0,0 +1,13 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lot /s
+del *.synctex.gz /s
+del *.synctex.gz(busy) /s
